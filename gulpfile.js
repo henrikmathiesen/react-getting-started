5aea9cd4a7660d40a44b595a08bd756281d2bf55
Henrik <henrik_mathiesen@hotmail.com>
2016-07-09 15:48:34 +0200

    -workflow set up

---
+++ gulpfile.js
@@ -0,0 +1,66 @@
+var gulp = require('gulp');
+
+var argv = require('yargs').argv;
+var gulpif = require('gulp-if');
+
+var sourceMaps = require('gulp-sourcemaps');
+var concatJs = require('gulp-concat');
+var uglifyJs = require('gulp-uglify');
+
+var babel = require("gulp-babel");
+
+//
+// Toggle production / debug builds (example gulp --prod)
+
+var isProduction = (argv.prod) ? (true) : (false);
+
+//
+// Sources
+
+var jsLibSrcDev = [
+    './bower_components/react/react.js',
+    './bower_components/react/react-dom.js'
+];
+
+// React complains about minifying the normal react files, we must use their minified versions (its "better"), so we set it up like this
+var jsLibSrcProd = [
+    './bower_components/react/react.min.js',
+    './bower_components/react/react-dom.min.js'
+];
+
+var jsLibSrc = isProduction ? jsLibSrcProd : jsLibSrcDev;
+
+var jsAppSrc = './app/js/src/**/*.js';
+var bldFolder = './app/js/bld';
+
+//
+// Tasks
+
+gulp.task('js-lib', function () {
+    return gulp
+        .src(jsLibSrc)
+        .pipe(gulpif(!isProduction, sourceMaps.init()))
+
+        .pipe(concatJs('lib.js'))
+        // no need to uglify after concat since they are minified already
+
+        .pipe(gulpif(!isProduction, sourceMaps.write()))
+        .pipe(gulp.dest(bldFolder));
+});
+
+gulp.task('js-app', function () {
+    return gulp
+        .src(jsAppSrc)
+        .pipe(gulpif(!isProduction, sourceMaps.init()))
+
+        .pipe(babel())
+
+        .pipe(concatJs('app.js'))
+
+        .pipe(gulpif(isProduction, uglifyJs()))
+
+        .pipe(gulpif(!isProduction, sourceMaps.write()))
+        .pipe(gulp.dest(bldFolder));
+});
+
+gulp.task('default', ['js-lib', 'js-app'], function () { });
