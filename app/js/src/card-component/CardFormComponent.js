3cf2f252a48dd91693856b98dadabae728c479e2
Henrik <henrik_mathiesen@hotmail.com>
2016-07-10 12:14:57 +0200

    -card component

--- app/js/src/card-component/CardFormComponent.js
+++ app/js/src/card-component/CardFormComponent.js
@@ -1,25 +1,16 @@
 var CardFormComponent = React.createClass({
 
-    getInitialState: function () {
-        return {
-            loginName: ''
-        };
-    },
-
     submitLoginName: function(e){
         e.preventDefault();
-        this.props.submitLoginName(this.state.loginName);
-    },
-
-    setLoginName: function(e){
-        this.setState({ loginName: e.target.value });
+        this.props.submitLoginName(this.refs.loginName.value);
+        this.refs.loginName.value = "";
     },
 
     render: function(){
         return(
             <form onSubmit={ this.submitLoginName }>
                 <div>
-                    <input type="text" placeholder="GitHub login" onChange={ this.setLoginName } />
+                    <input type="text" placeholder="GitHub login" ref="loginName" />
                 </div>
                 <br />
                 <div>
