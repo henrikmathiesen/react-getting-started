23f167acce8e52dea0bc800efcffeefb5b0da62b
Henrik <henrik_mathiesen@hotmail.com>
2016-07-10 10:43:28 +0200

    -begin work on card component

---
+++ app/js/src/card-component/CardComponentMain.js
@@ -0,0 +1,13 @@
+var CardComponentMain = React.createClass({
+
+    render: function(){
+        return(
+            <div>
+                It works
+            </div>
+        )
+    }
+
+});
+
+ReactDOM.render(<CardComponentMain />, document.getElementById('card-component'));
